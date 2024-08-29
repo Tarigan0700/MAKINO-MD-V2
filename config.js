7853b5cf19586808df3ac1703cc51c389f6933b3
DO <72868704+Tarigan0700@users.noreply.github.com>
2024-08-29 18:45:40 +0700

    Update config.js

--- config.js
+++ config.js
@@ -19,8 +19,8 @@ global.ANTI_BOT = false;
 global.PM_BLOCKER = false;
 global.CHATBOT = false;
 
-global.SESSION_ID = process.env.SESSION_ID || ""
-global.Owner = ["2348138686232"]; //like 2347080968564 
+global.SESSION_ID = process.env.SESSION_ID || "V8NRDRL8"
+global.Owner = ["6281375235713"]; //like 2347080968564 
 global.OwnerName =  "Tᴀɪʀᴀ Mᴀᴋɪɴᴏ";
 global.BotName = "♱MAKINO-MD-V2♱♡⃤";
 global.packname = "♱MAKINO-MD-V2♱♡⃤";                             //Do not change.
